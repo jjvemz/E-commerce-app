7450ceee4df82498153f97519f9672f8aa4fa35f
juanjo2331 <40677444+jjvemz@users.noreply.github.com>
2023-04-03 19:43:30 -0400

    modified:   jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -14,7 +14,7 @@ module.exports = {
       host: "localhost",
       port: 3306,
       username: "root",
-      password: "root1234",
+      password: process.env.DB_PASSWORD,
       database: "mydatabase",
       logging: false,
     },
